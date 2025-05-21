1a034f0fdd7db8f4f3d4d136567fa1caf238373c
kontty08 <152802885+kontty08@users.noreply.github.com>
2025-05-21 14:54:32 +0900

    first commit

---
+++ next.config.js
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' blob:; style-src 'self' 'unsafe-inline' https:; img-src 'self' data: https: blob:; font-src 'self' data: https:; connect-src 'self' https:; worker-src 'self' blob:;"
+          }
+        ]
+      }
+    ];
+  },
+}
+
+module.exports = nextConfig
